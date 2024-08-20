6b4cdd2e5920de38c34062c2c00b49987afd93ad
mecaneer23 <mecaneer23@gmail.com>
2024-08-20 11:13:58 -0500

    feat: completely rework css and adding logic

--- functions.js
+++ functions.js
@@ -5,29 +5,20 @@ function addOne(counter) {
 function subtractOne(counter) {
     counter.value = parseInt(counter.value) - 1
 }
-function addCounters() {
+
+function addCounter() {
     let original = document.querySelector(".counter");
     let container = document.getElementById("container");
-    let howMany = parseInt(document.getElementById("how-many").value);
-    if (howMany >= 0) {
-        for (let i = 0; i < howMany; i++) {
-            const newNode = original.cloneNode(true);
-            newNode.children[0].children[0].placeholder = "Counter " + (container.children.length + 1);
-            newNode.children[0].children[0].value = "";
-            newNode.children[1].value = "0";
-            container.appendChild(newNode);
-        }
-        return;
-    }
-    if (container.children.length > -howMany) {
-        for (let i = 0; i < -howMany; i++) {
-            container.removeChild(container.children[container.children.length - 1]);
-        }
-    }
+    const newNode = original.cloneNode(true);
+    newNode.children[0].children[0].placeholder = "Counter " + container.children.length;
+    newNode.children[0].children[0].value = "";
+    newNode.children[1].value = "0";
+    container.insertBefore(newNode, container.children[container.childElementCount - 1]);
 }
+
 function deleteCounter(counter) {
     let container = document.getElementById("container");
-    if (container.children.length > 1) {
+    if (container.children.length > 2) {
         container.removeChild(counter);
     }
 }
