c893237e2ce5014397103f682bec523cc1769c8a
mecaneer23 <mecaneer23@gmail.com>
2024-10-05 03:12:29 -0500

    feat: move add button out of container

--- functions.js
+++ functions.js
@@ -10,15 +10,17 @@ function addCounter() {
     let original = document.querySelector(".counter");
     let container = document.getElementById("container");
     const newNode = original.cloneNode(true);
-    newNode.children[0].children[0].placeholder = "Counter " + container.children.length;
+    newNode.children[0].children[0].placeholder = "Counter " + (container.children.length + 1);
     newNode.children[0].children[0].innerHTML = "";
     newNode.children[1].innerHTML = "0";
-    container.insertBefore(newNode, container.children[container.childElementCount - 1]);
+    container.append(newNode);
 }
 
 function deleteCounter(counter) {
     let container = document.getElementById("container");
-    if (container.children.length > 2) {
-        container.removeChild(counter);
+    if (container.children.length == 1) {
+        alert("At least one counter must remain.")
+        return;
     }
+    container.removeChild(counter);
 }
